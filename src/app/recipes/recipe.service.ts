fa5f4ddf3f78f712da9a5b9cacea50b9028fe8ae
Karan Bhavsar <bhavsar.ka@husky.neu.edu>
2018-08-17 10:41:15 -0400

    angular first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      '420 Burger',
+      'This burger is stacked with all the best fried food',
+      'https://mnprairieroots.files.wordpress.com/2016/06/davis-square-352-the-420-burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+
+      ]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
